b493651fb517f439684cd2858bf838bff1856059
mateuszo19 <mateusz_palanis@vp.pl>
2025-01-14 20:12:52 +0000

    update_date update

--- update_date.py
+++ update_date.py
@@ -1,25 +1,21 @@
 # -*- coding: utf-8 -*-
 
 import os
+import time
 from datetime import datetime
 from subprocess import call
 
 def update_date_file():
-    # Ścieżka do pliku
     folder_path = "data/raw"
     date_file = os.path.join(folder_path, "date.csv")
 
-    # Upewnij się, że folder istnieje
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
 
-    # Pobierz aktualną datę i czas
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    # Sprawdź, czy plik istnieje, aby dodać nagłówki tylko raz
     file_exists = os.path.isfile(date_file)
 
-    # Zapisz datę do pliku
     with open(date_file, "a") as file:
         if not file_exists:
             file.write("Timestamp\n")  # Nagłówek CSV
@@ -28,21 +24,27 @@ def update_date_file():
     print("Plik {} został zaktualizowany.".format(date_file))
 
 def git_commit_and_push():
-    # Ścieżka do pliku
+    os.chdir("/root/gitapp")
+    git_path = "/usr/bin/git"
     date_file = "data/raw/date.csv"
 
-    # Wykonaj commit i push
-    call(["git", "add", date_file])
-    call(["git", "commit", "-m", "Train model"])
-    call(["git", "push"])
+    call([git_path, "add", date_file])
+    call([git_path, "commit", "-m", "Model training"])
+    call([git_path, "push"])
     print("Zaktualizowany plik został wysłany na GitHub.")
 
 def main():
-    try:
-        update_date_file()
-        git_commit_and_push()
-    except Exception as e:
-        print("Wystąpił błąd: {}".format(e))
+    while True:
+        try:
+            update_date_file()
+            git_commit_and_push()
+        except Exception as e:
+            print("Wystąpił błąd: {}".format(e))
+        
+        # Oczekuj losową ilość czasu przed kolejnym wykonaniem (np. 3-6 godzin)
+        wait_time = 3 * 3600 + (time.time() % 10800)  # 3-6 godzin (w sekundach)
+        print("Czekam przez {} sekund.".format(wait_time))
+        time.sleep(wait_time)
 
 if __name__ == "__main__":
     main()
